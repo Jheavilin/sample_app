71c776337933ec6266347f9610df9d9eb38cb237
jheavilin <jheavilin@gmail.com>
2012-09-06 12:20:21 -0600

    Fixed the corrupted files routes.rb and secrete_tockens.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
 SampleApp::Application.config.secret_token = '15fb1d87b1f1e44555c90fb0b4eaaf3f0f08bea9a4b44eb5f7ea41bf3d4ffdfa8e3fd5d703740859dc2b9b58052512b2e47ae4b1b751421d21810580c2a82907'
-=======
 SampleApp::Application.config.secret_token = '2440a593a150925dc7cca9e5e11f7e685a47573889d19c8d83e00a51db30b9e3fdfa2730559bff9a036d887b8d72518bc2b0d98d3a7306eaff224df9f1abadd0'
->>>>>>> ee055290bb05ff67d00549f02d57352629c1d724
